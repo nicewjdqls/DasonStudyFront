674b8c0f923ccf0db1fef30b452466273db06210
koo <nicewdjql@naver.com>
2024-11-06 10:55:24 +0900

    aws lamda1

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const api = axios.create({
     // Server URL
     // baseURL: 'http://localhost:5000/api',
-    baseURL: 'https://hkz9nqql2m.execute-api.ap-northeast-2.amazonaws.com/api',
+    baseURL: 'https://v5t9tob6ga.execute-api.ap-northeast-2.amazonaws.com/api',
     headers: {
         "Content-Type": "application/json",
       },
